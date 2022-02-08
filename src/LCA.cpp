8472a47986999f20f4d5cb7d88232fa4b15d2638
HeSH <neogate@sogang.ac.kr>
2022-02-08 14:07:38 +0900

    Update LCA.cpp

--- src/LCA.cpp
+++ src/LCA.cpp
@@ -1,25 +1,25 @@
-// lev[]: 트리 정점의 레벨저장, lca[x][i] = x의 2^i번째 조상 저장
+// d[]: 트리 정점의 레벨저장, par[x][i] = x의 2^i번째 조상 저장
 void dfs(ll cur, ll pre) {
-	lev[cur] = lev[pre] + 1;
-	lca[cur][0] = pre;
+	d[cur] = d[pre] + 1;
+	par[cur][0] = pre;
 	for (auto i : adj[cur]) {
 		if (i == pre)continue;
-		lca[i][0] = cur;
+		par[i][0] = cur;
 		dfs(i, cur);
 	}
 }
 
 ll query(ll x, ll y) {
-	if (lev[x] < lev[y])swap(x, y);
+	if (d[x] < d[y])d(x, y);
 	for (int i = 20; i >= 0; i--) {
-		if (lev[x] - lev[y] >= (1 << i)) x = lca[x][i];
+		if (d[x] - d[y] >= (1 << i)) x = par[x][i];
 	}
 	if (x == y)return x;
 	for (int i = 20; i >= 0; i--) {
-		if (lca[x][i] != lca[y][i]) {
-			x = lca[x][i];
-			y = lca[y][i];
+		if (par[x][i] != par[y][i]) {
+			x = par[x][i];
+			y = par[y][i];
 		}
 	}
-	return lca[x][0];
+	return par[x][0];
 }
