72795f2abb89177ae8e3b874527569fda8521a90
HeSH <neogate@sogang.ac.kr>
2022-02-09 17:56:56 +0900

    Update 7616.cpp

--- 7616.cpp
+++ 7616.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 typedef int ll;
 ll n, k, tc = 1, cache[10101];
-bool flow[10101][10101], cap[10101][10101];
-set<ll>adj[10101];
+ll flow[10101][10101], cap[10101][10101];
+vector<ll>adj[10101];
 string s;
 void edmond_karp(ll source, ll sink) {
 	ll max_flow = 0;
@@ -21,7 +21,7 @@ void edmond_karp(ll source, ll sink) {
 		while (!q.empty()) {
 			ll cur = q.front(); q.pop();
 			for (auto i : adj[cur]) {
-				if (cache[i] == -1 && cap[cur][i]&&!flow[cur][i]) {
+				if (cache[i] == -1 && (cap[cur][i] - flow[cur][i]) > 0) {
 					cache[i] = cur;
 					q.push(i);
 					if (i == sink)break;
@@ -29,18 +29,24 @@ void edmond_karp(ll source, ll sink) {
 			}
 		}
 		if (cache[sink] == -1)break;
-		max_flow += 1;
+		ll min_cap = 1e9;
+		for (ll i = sink; i != source; i = cache[i]) {
+			ll bef = cache[i];
+			min_cap = min(min_cap, cap[bef][i] - flow[bef][i]);
+		}
+		max_flow += min_cap;
 		for (ll i = sink; i != source; i = cache[i]) {
 			if (i <= n)curans.push_back(i);
 			ll bef = cache[i];
-			flow[bef][i] = true;
-			flow[i][bef] = false;
+			flow[bef][i] += min_cap;
+			flow[i][bef] -= min_cap;
 		}
 		curans.push_back(1);
 		reverse(curans.begin(), curans.end());
 		ans.push_back(curans);
 	}
-	if (max_flow <k)cout << "Impossible" << endl;
+	cout << "max flow is " << max_flow << endl;
+	if (max_flow < k)cout << "Impossible" << endl;
 	else {
 		ll cnt = 0;
 		for (auto v : ans) {
@@ -58,8 +64,7 @@ int main() {
 	while (1) {
 		cin >> k >> n;
 		if (n == 0 && k == 0)break;
-		for (int i = 0; i < 5050; i++) adj[i].clear();
-		memset(cache, 0, sizeof(cache));
+		for (int i = 0; i < 10101; i++) adj[i].clear();
 		getline(cin, s);
 		for (int i = 1; i <= n; i++) {
 			getline(cin, s);
@@ -69,28 +74,28 @@ int main() {
 				else {
 					ll cur = stoi(asdf);
 					asdf.clear();
-					adj[i+n].insert(cur);
-					adj[cur+n].insert(i);
-					cap[i+n][cur] = true;
-					cap[cur+n][i] = true;
+					adj[i + n].push_back(cur);
+					adj[cur].push_back(i+n);
+					cap[i + n][cur] = 1;
+					cap[cur][i + n] = 1;
 				}
 			}
 			if (!asdf.empty()) {
 				ll cur = stoi(asdf);
 				asdf.clear();
-				adj[i + n].insert(cur);
-				adj[cur + n].insert(i);
-				cap[i + n][cur] = true;
-				cap[cur + n][i] = true;
+				adj[i + n].push_back(cur);
+				adj[cur].push_back(i + n);
+				cap[i + n][cur] = 1;
+				cap[cur][i + n] = 1;
 			}
 		}
 		cout << "Case " << tc++ << ":" << endl;
-		for (ll i = 1; i <= n; i++){
-			adj[i].insert(i + n);
-			adj[i + n].insert(i);
+		for (ll i = 1; i <= n; i++) {
+			adj[i].push_back(i + n);
+			adj[i + n].push_back(i);
 			if (i <= 2) continue;
-			cap[i][i + n] = true;
-			cap[i + n][i] = true;
+			cap[i][i + n] = 1;
+			cap[i + n][i] = 1;
 		}
 		edmond_karp(1 + n, 2);
 		cout << endl;
