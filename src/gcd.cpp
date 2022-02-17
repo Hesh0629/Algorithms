4f4cb42e349cabe3b9f0c9a1e50cb1920985fc2f
HeSH <neogate@sogang.ac.kr>
2022-02-17 23:13:47 +0900

    Update gcd.cpp

--- src/gcd.cpp
+++ src/gcd.cpp
@@ -3,6 +3,8 @@ ll gcd(ll x,ll y){
   return gcd(y,x%y);
 }
 
+// ax-by= gcd(a,b)의 해를 구하고 싶더라도 똑타이 ex_gcd(a,b)를 진행하면 된다.
+// 당연하게도 y가 음수인 해를 구하면 되는것이기 
 pair<ll,ll> ex_gcd(ll a,ll b){
   if(!b) return{1,0};
   pair<ll,ll>rec=ex_gcd(b,a%b);
