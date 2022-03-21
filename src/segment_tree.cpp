eaa34b038014e9c5d3535614a323fcc9a2b1a387
HeSH <neogate@sogang.ac.kr>
2022-03-21 11:36:09 +0900

    Update segment_tree.cpp

--- src/segment_tree.cpp
+++ src/segment_tree.cpp
@@ -1,15 +1,18 @@
-ll init(ll N, ll cx, ll cy) {
-	if (cx == cy) return segtree[N] = v[cx];
+void init(ll N, ll cx, ll cy) {
+	if (cx == cy) {
+		segtree[N] = v[cx];
+		return;
+	}
 	ll mid = (cx + cy) / 2;
 	init(N * 2, cx, mid);
 	init(N * 2 + 1, mid + 1, cy);
-	return segtree[N] = segtree[N * 2] * segtree[N * 2 + 1];
+	segtree[N] = segtree[N * 2] + segtree[N * 2 + 1];
 }
 ll query(ll N, ll cx, ll cy, ll tx, ll ty) {
-	if (cy < tx || ty < cx)return 1;
+	if (cy < tx || ty < cx)return 0;
 	if (tx <= cx && cy <= ty)return segtree[N];
 	ll mid = (cx + cy) / 2;
-	return query(N * 2, cx, mid, tx, ty) * query(N * 2 + 1, mid + 1, cy, tx, ty);
+	return query(N * 2, cx, mid, tx, ty) + query(N * 2 + 1, mid + 1, cy, tx, ty);
 }
 void update(ll N, ll cx, ll cy, ll idx, ll val) {
 	if (idx < cx || cy < idx)return;
@@ -20,5 +23,5 @@ void update(ll N, ll cx, ll cy, ll idx, ll val) {
 	ll mid = (cx + cy) / 2;
 	update(N * 2, cx, mid, idx, val);
 	update(N * 2 + 1, mid + 1, cy, idx, val);
-	segtree[N] = segtree[N * 2] * segtree[N * 2 + 1];
+	segtree[N] = segtree[N * 2] + segtree[N * 2 + 1];
 }
